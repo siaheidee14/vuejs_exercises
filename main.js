c6c75ebc23aff9835ea5274e0a1db6b1a970f4d7
heideesia <siaheidee@gmail.com>
2018-08-10 12:40:19 +0800

    First Commit on Vuejs tutorial

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        images: 'images/s01.jpeg',
+        href: 'https:shopee.com',
+        alt: 'Eco Friendly Socks',
+        onSale: false,
+        details: ["80% Cotton", "20% Polyester", "Gender-neutral"]
+    }
+})
